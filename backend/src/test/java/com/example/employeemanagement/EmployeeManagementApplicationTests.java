ce5ced312b5c37080f2fa026f9ed7d5fc830ba92
Son Nguyen <hoangson091104@gmail.com>
2024-09-05 17:59:19 -0400

    Fix: Addressed security vulnerabilities

--- backend/src/test/java/com/example/employeemanagement/EmployeeManagementApplicationTests.java
+++ backend/src/test/java/com/example/employeemanagement/EmployeeManagementApplicationTests.java
@@ -9,11 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * This class implements unit tests for the EmployeeManagementApplication.
+ */
 @DataJpaTest
 @Transactional
 public class EmployeeManagementApplicationTests {
