7e49498ad17c79a0dcc1d45ef1f1679092099068
Son Nguyen <hoangson091104@gmail.com>
2024-09-05 17:54:54 -0400

    Fix: Addressed security vulnerabilities

--- backend/src/main/java/com/example/employeemanagement/controller/DepartmentController.java
+++ backend/src/main/java/com/example/employeemanagement/controller/DepartmentController.java
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 /**
