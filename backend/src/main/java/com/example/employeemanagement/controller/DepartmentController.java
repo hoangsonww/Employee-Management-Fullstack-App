39cdecebdb0472f617199f7c60c0f517e8fb2913
Son Nguyen <hoangson091104@gmail.com>
2024-09-05 18:08:00 -0400

    Fix: Addressed security vulnerabilities

--- backend/src/main/java/com/example/employeemanagement/controller/DepartmentController.java
+++ backend/src/main/java/com/example/employeemanagement/controller/DepartmentController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/departments")
 @CrossOrigin(origins = "http://localhost:3000")
+@Tag(name = "Department APIs", description = "Operations related to managing departments")
 public class DepartmentController {
 
   @Autowired private DepartmentService departmentService;
