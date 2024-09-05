ace787d398cea12f72a9da747b3f85c3805e7583
Son Nguyen <hoangson091104@gmail.com>
2024-09-05 18:01:55 -0400

    Fix: Addressed security vulnerabilities

--- backend/src/main/java/com/example/employeemanagement/config/CorsConfig.java
+++ backend/src/main/java/com/example/employeemanagement/config/CorsConfig.java
@@ -5,12 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/** This class represents the configuration for CORS. */
 @Configuration
 public class CorsConfig {
 
+  /**
+   * Configure CORS.
+   *
+   * @return WebMvcConfigurer
+   */
   @Bean
   public WebMvcConfigurer corsConfigurer() {
     return new WebMvcConfigurer() {
+
+      /**
+       * Add CORS mappings.
+       *
+       * @param registry CORS registry
+       */
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
