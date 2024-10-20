495ae4c80c73338b5dbd5761c9271f82410175ef
Son Nguyen <hoangson091104@gmail.com>
2024-10-20 13:00:49 -0400

    Fix: Deployed Backend with Docker & Render

--- backend/src/main/java/com/example/employeemanagement/EmployeeManagementApplication.java
+++ backend/src/main/java/com/example/employeemanagement/EmployeeManagementApplication.java
@@ -3,17 +3,30 @@ package com.example.employeemanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
 /** This class represents the main entry point of the application. */
 @SpringBootApplication
 @OpenAPIDefinition(
     info =
-        @Info(
-            title = "Employee Management System API",
-            version = "1.0.0",
-            description = "API documentation for a system managing employees and departments")
-        )
+    @Info(
+        title = "Employee Management System API",
+        version = "1.0.0",
+        description = "API documentation for managing employees and departments",
+        contact = @Contact(
+            name = "Employee Management System",
+            email = "hoangson091104@gmail.com",
+            url = "https://employee-management-fullstack-app.vercel.app/"
+        ),
+        license = @License(
+            name = "MIT License",
+            url = "https://opensource.org/licenses/MIT"
+        ),
+        termsOfService = "https://employee-management-fullstack-app.vercel.app/"
+    )
+)
 public class EmployeeManagementApplication {
 
   /**
